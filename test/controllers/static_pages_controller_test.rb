b7bfc7243370056c8df811c1efce6127f35e392f
EC2 Default User <ec2-user@ip-172-31-39-123.us-east-2.compute.internal>
2018-08-01 12:20:44 +0000

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,5 +1,3 @@
-require 'test_helper'
-
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
@@ -14,25 +12,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", @base_title
   end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", @base_title
   end
   
   test "shoud get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", @base_title
   end
 
+  
 end
