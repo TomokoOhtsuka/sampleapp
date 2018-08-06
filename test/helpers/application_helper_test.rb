df851ad02dcb4bf1bd327b59c9fb2c1f9a152d18
EC2 Default User <ec2-user@ip-172-31-39-123.us-east-2.compute.internal>
2018-08-06 11:56:50 +0000

    end chap5

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title, "Ruby on Rails Tutorial Sampleapp"
-    assert_equal full_title("Help"), "Help | #{@base_title}"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sampleapp"
   end
 end
