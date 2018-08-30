e9c5b0969bcc3679958ea6372bf6e92b84b74804
EC2 Default User <ec2-user@ip-172-31-39-123.us-east-2.compute.internal>
2018-08-30 08:56:12 +0000

    finish 13.4.2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,12 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
+  
   
   def create 
-    @micrpost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      rediret_to root_url
+      redirect_to root_url
     else
       @feed_items = []
       render "static_pages/home"
@@ -12,12 +14,23 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+    #redirect_back(fallback_location: root_url)　←rails5から新たに導入されたやり方
   end
   
   private
   
   def micropost_params
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content, :picture)
   end
+  
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
+  
+  
   
 end
